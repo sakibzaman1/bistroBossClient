e73491b23882de0472694ba367d7af9815f656c7
sakibzaman1 <sakibzaman20@gmail.com>
2024-03-28 22:01:12 +0600

    Login Page has been completed

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../Providers/AuthProvider';
+import { BiLowVision, BiShowAlt } from "react-icons/bi";
+import { FcGoogle } from "react-icons/fc";
+import loginImg from '../../assets/others/authentication2.png'
 
 const Login = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginError, setLoginError] = useState("");
+
+  const { signInUser, signInWithGoogle, goToTop } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
 
     const [disabled, setDisabled] = useState(true)
 
@@ -10,36 +22,76 @@ const Login = () => {
     useEffect(()=> {
         loadCaptchaEnginge(6)
     },[])
-    const handleLogin = e => {
-        e.preventDefault()
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
+    const handleLogin = (e) => {
+      e.preventDefault();
+      const form = e.target;
+      const email = form.email.value;
+      const password = form.password.value;
+      console.log(email, password);
+      setLoginError("");
+  
+      signInUser(email, password)
+        .then(() => {
+          swal({
+            position: "top-center",
+            icon: "success",
+            title: "Successfully Signed In",
+            showConfirmButton: false,
+            showCancelButton: false,
+            timer: 2000,
+          });
+          // navigate user
+          setTimeout(() => {
+            navigate(location?.state ? location.state : "/");
+          }, 2000);
+        })
+        .catch(() => {
+          setLoginError("Invalid User. Please Check Email or Password Again");
+        });
+    };
 
-    }
+    const handleGoogleSignIn = () => {
+      signInWithGoogle()
+        .then(() => {
+          swal({
+            position: "top-center",
+            icon: "success",
+            title: "Successfully Logged In",
+            showConfirmButton: false,
+            showCancelButton: false,
+            timer: 2000,
+          });
+          // navigate user
+          setTimeout(() => {
+            navigate(location?.state ? location.state : "/");
+          }, 2000);
+        })
+        .catch((error) => {
+          setLoginError(error.message);
+        });
+  }
 
-    const handleValidateCaptcha = () => {
-      const userCaptchaValue = captchaRef.current.value;
-      
-      if(validateCaptcha(userCaptchaValue)){
-        alert('Captcha MAtched');
-        setDisabled(false);
-      }
-      else{
-        alert('Captcha Not Matched')
-        setDisabled(true)
-      }
+  const handleValidateCaptcha = () => {
+    const userCaptchaValue = captchaRef.current.value;
+    
+    if(validateCaptcha(userCaptchaValue)){
+      alert('Captcha MAtched');
+      setDisabled(false);
     }
+    else{
+      alert('Captcha Not Matched')
+      setDisabled(true)
+    }
+  }
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row">
+            <div className="hero text-sm py-6 px-20 bg-base-200">
+  <div className="hero-content flex-col lg:flex-row gap-28">
     <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <img src={loginImg} alt="" />
     </div>
-    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl h-2/3 bg-base-100">
+      <h1 className='text-3xl text-center my-2'>Login</h1>
       <form onSubmit={handleLogin} className="card-body">
         <div className="form-control">
           <label className="label">
@@ -48,14 +100,20 @@ const Login = () => {
           <input type="email" placeholder="email" name='email' className="input input-bordered" required />
         </div>
         <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <div className="flex items-center relative">
+                                    <input name="password" type={showPassword ? "text" : "password"} placeholder="password" className="input input-bordered w-full" required />
+                                    <div className="absolute right-4">
+                                        {
+                                            !showPassword ? <BiLowVision className="cursor-pointer" size="20px" onClick={() => setShowPassword(!showPassword)}></BiLowVision> : <BiShowAlt className="cursor-pointer" size="20px" onClick={() => setShowPassword(!showPassword)}></BiShowAlt>
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+
+                            
         <div className="form-control">
           <label className="label">
           <LoadCanvasTemplate />
@@ -63,9 +121,24 @@ const Login = () => {
           <input type="Type the captcha above" ref={captchaRef} placeholder="Captcha" name='captcha' className="input input-bordered" required />
           <button onClick={handleValidateCaptcha} className="btn btn-xs my-4">Validate</button>
         </div>
-        <div className="form-control mt-6">
+        <div className="form-control mt-2">
           <button disabled={disabled} className="btn btn-primary">Login</button>
         </div>
+        <div className="mx-auto mb-4 pt-4  w-full px-10 text-center">
+                                {
+                                    loginError && <p className="text-rose-800 italic text-bold">{loginError}</p>
+                                }
+                            </div>
+        <div className="flex flex-col justify-between mt-10 mb-6 space-y-6 lg:space-y-0">
+                                <button onClick={handleGoogleSignIn} className="text-black btn btn-outline rounded-none bg-transparent hover:rounded-none capitalize">
+                                    <FcGoogle></FcGoogle>
+                                    Sign in with Google
+                                </button>
+                            </div>
+
+                            <div className="text-center pt-6">
+                                <small className="font-medium text-black">New to this Website? Please<Link to="/register" className="text-red-700 ml-1"><button onClick={goToTop}>Register</button></Link></small>
+                            </div>
       </form>
     </div>
   </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/router.jsx'
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Providers/AuthProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   <AuthProvider>
    <HelmetProvider>
    <div className='max-w-screen-xl mx-auto'>
     <RouterProvider router={router} />
     </div>
    </HelmetProvider>
+   </AuthProvider>
   </React.StrictMode>,
 )
